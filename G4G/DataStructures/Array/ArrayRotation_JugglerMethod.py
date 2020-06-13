da081f1e67707d6c6edb40d344d65aee0c4c7db3
Rajkumar patel <rajkumar.patel@seagate.com>
2020-06-13 19:26:36 +0530

    Add array rotation jugglermethod with O(n)

---
+++ G4G/DataStructures/Array/ArrayRotation_JugglerMethod.py
@@ -0,0 +1,49 @@
+# Python3 program to rotate an array by
+# d elements
+# Function to left rotate arr[] of size n by d*/
+
+# jugging method is used.
+
+# time complexity O(size of an array)
+# space complexity O(1)
+
+# utility function to print an array */
+
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def printArray(arr, size):
+    for i in range(size):
+        print (str(arr[i])+" ")
+
+
+def rotateLeft(arr, d, n):
+    d = gcd(d, n)
+    for i in range(d):
+        temp = arr[i]
+        j = i
+        while 1:
+            k = j+d
+            if k < n:
+                arr[j] = arr[k]
+                j = k
+            else:
+                break
+        arr[j] = temp
+
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def main():
+    rotation = input("give number of rotation needed!")
+    rotateLeft(arr, rotation, 10)
+    printArray(arr, 10)
+
+
+if __name__ == "__main__":
+    main()
